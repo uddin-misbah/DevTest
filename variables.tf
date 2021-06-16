23c71fc81041cd45eb0d7d9594e28b6d27d80721
mirkaay <53059374+mirkaay@users.noreply.github.com>
2021-06-16 00:27:15 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  description = "golang-misbah"
+  type        = string
+}
+
+variable "bucket_name" {
+  default = "actions_bucket_misbah"
+  type        = string
+}
+
+variable "topic_name" {
+  description = "The topic in Pub/Sub that receives notifications."
+  type        = string
+}
+
+variable "event_type" {
+  description = "The events that trigger a notification to be sent."
+  type        = string
+  default     = "OBJECT_FINALIZE"
+}
