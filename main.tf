b3e4ed2d9f9baf19dc2c72595b1dbe13f11d3250
mirkaay <53059374+mirkaay@users.noreply.github.com>
2021-06-16 13:20:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,16 @@
-data "google_storage_project_service_account" "gcs_service_account" {
+resource "google_service_account" "service_account" {
+  account_id   = "misbahuddin-sa-pubsub"
+  display_name = "misbahuddin-sa-pubsub"
   project = var.project_id
 }
 
 resource "google_pubsub_topic" "pubsub" {
   name = var.topic_name
-}
-
-resource "google_storage_notification" "notification" {
-  bucket         = var.bucket_name
-  payload_format = "JSON_API_V1"
-  topic          = var.topic_name
-  event_types    = [var.event_type]
-
-  depends_on = [google_pubsub_topic_iam_binding.binding]
+  project = var.project_id
 }
 
 resource "google_pubsub_topic_iam_binding" "binding" {
-  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_service_account.email_address}"]
+  members = ["serviceAccount:${google_service_account.service_account.email_address}"]
   role    = "roles/pubsub.publisher"
   topic   = google_pubsub_topic.pubsub.id
 }
